705a834277c02eff4d75fba24fd29f4178731bc3
jgarciapueyo <jgarciapueyo@gmail.com>
2019-02-22 12:35:56 +0100

    Presentation first part

--- solution.py
+++ solution.py
@@ -1,24 +1,25 @@
 import csv
 import numpy as np
-import os
 import random
 
 def read_input(dir, filename):
-  with open(dir+"/"+filename+".in", 'r') as file:
+  with open(dir+"/"+filename+".in", 'r') as file: 
     csv_reader = csv.reader(file, delimiter=' ')
     rows, columns, min, max = next(csv_reader)
-    pizza = [list(line[0]) for line in csv_reader]
+    pizza = [list(line[0]) for line in csv_reader] 
   return int(rows), int(columns), int(min), int(max), np.array(pizza)
+
+import os
 
 def write_output(correct_slices, dir, filename):
     if not os.path.exists(dir):
         os.makedirs(DIRECTORY)
-    
-    with open(dir+"/s_"+filename+".in", 'w+') as out: 
+
+    with open(dir+"/s_"+filename+".in", 'w+') as out:
         total = len(correct_slices)
         out.write(str(total)+"\n")
         for sli in correct_slices:
-            out.write(' '.join(map(str, sli))+"\n")   
+            out.write(' '.join(map(str, sli))+"\n")  
         print(total)
 
 def random_cuts(ROWS, COLUMNS, H):
@@ -32,7 +33,6 @@ def random_cuts(ROWS, COLUMNS, H):
 
 def correct_slice(pizza, r1, c1, r2, c2, R, C, L, H):
   try_slice = pizza[r1:r2+1, c1:c2+1]
-  
   if 'x' in try_slice:
     return False
   else:
@@ -49,21 +49,25 @@ def cut_slice(pizza, r1, c1, r2, c2):
     for i in range(r1, r2+1):
         for j in range(c1, c2+1):
             pizza[i][j] = 'x'
-            
-DIRECTORY = "hashcode"
-FILENAME = "c_medium"
-NUM_RAND_COORD = 10000
 
-ROWS, COLUMNS, L, H, PIZZA = read_input(DIRECTORY, FILENAME)
+def main(directory, filename):
+  NUM_RAND_COORD = 10000
+  ROWS, COLUMNS, L, H, PIZZA = read_input(directory, filename)
+  correct_slices = []
+  
+  for _ in range(NUM_RAND_COORD):
+    r1, c1, r2, c2 = random_cuts(ROWS, COLUMNS, H)
 
-correct_slices = []
-
-for _ in range(NUM_RAND_COORD):
-  r1, c1, r2, c2 = random_cuts(ROWS, COLUMNS, H)
-
-  if correct_slice(PIZZA, r1, c1, r2, c2, ROWS, COLUMNS, L, H):
-    cut_slice(PIZZA, r1, c1, r2, c2)
-    sli = [r1, c1, r2, c2]
-    correct_slices.append(sli)
+    if correct_slice(PIZZA, r1, c1, r2, c2, ROWS, COLUMNS, L, H):
+      cut_slice(PIZZA, r1, c1, r2, c2)
+      sli = [r1, c1, r2, c2]
+      correct_slices.append(sli)
     
-write_output(correct_slices, DIRECTORY, FILENAME)
+  write_output(correct_slices, directory, filename)
+
+if __name__ == '__main__':
+  MODES = ['a_example', 'b_small', 'c_medium', 'd_big']
+  DIRECTORY = 'hashcode'
+
+  for mode in MODES:
+    main(DIRECTORY, mode)
